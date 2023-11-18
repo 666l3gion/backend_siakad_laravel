c7b4f2a16c3c7b202b6cfa3b3463ab9f9f9f346f
666l3gion <syawalaprian02@gmail.com>
2023-11-18 10:56:32 +0900

    pages auth and home ready

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2023 <div class="bullet"></div> Design By <a href="https://syawal.web.id/">Syawal Aprian</a>
+    </div>
+    <div class="footer-right">
+        0.0.1
+    </div>
+</footer>
